ae2593cc39d7a2a9079f096d2b9fa1dbcf8d55ad
Jenn D <dube.jenn@gmail.com>
2018-01-10 22:00:05 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Create cards for memory game
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//Display value that will indicate which cards user has flipped
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
