c9483f7f536e962ac2b17fb3dc2cfe37edf7b8f6
Jenn D <dube.jenn@gmail.com>
2018-01-17 00:33:35 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 //Create cards for memory game
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-//Display value that will indicate which cards user has flipped
+//Create an empty array for cards that will be in play
+var cardsInPlay = [];
+
+//Create card that user first flips
+var cardOne = cards[0];
+//Add first card to the cardsInPlay array
+cardsInPlay.push(cardOne);
+// Display the card that the user flipped
+console.log("User flipped " + cardOne);
+
+// Create card that user flips second
+var cardTwo = cards[2];
+//Add second card to cardsInPlay array
+cardsInPlay.push(cardTwo);
+// Display card that user flipped
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+
+// Check to see if user has played two cards
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+  alert("Sorry, try again.");
+}
+}
